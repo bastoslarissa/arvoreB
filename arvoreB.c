e13d88c403ae1e70319f4a979772e7509154455d
Giovanna Sona <giovannasona@ufpr.br>
2026-04-04 12:37:52 -0300

    add make e funcoes .c

--- arvoreB.c
+++ arvoreB.c
@@ -1,1 +1,13 @@
 #include "arvoreB.h"
+
+struct arvoreB* criarArvoreB(int32_t t_arvore) {
+
+}
+
+void inserirArvoreB(struct arvoreB* arvore, int32_t chave) {
+
+}
+
+void imprimirArvoreB(struct arvoreB* arvore) {
+  
+}
